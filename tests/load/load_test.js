9a1b62492902962f8612454e176c345d5f18fa6a
Deybi-hud <deyb.ramirez@duocuc.cl>
2026-06-26 02:49:36 -0400

    test: Configurar URL oficial de Amazon ELB en scripts k6 y Locust

--- tests/load/load_test.js
+++ tests/load/load_test.js
@@ -14,7 +14,7 @@ export const options = {
 };
 
 export default function () {
-  const res = http.get('http://localhost:8080/');
+  const res = http.get('http://a1723d010bdf949b5add817e3279197e-2015793512.us-east-1.elb.amazonaws.com/');
   check(res, { 'status was 200': (r) => r.status == 200 });
   sleep(1);
 }
